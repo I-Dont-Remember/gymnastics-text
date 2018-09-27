4cf574074b8b8b1e6a47ef5da8a7cfd71de72d56
Austin Meyer <frizzkitten@gmail.com>
2018-09-26 20:36:51 -0500

    set up textHandler.js

--- index.js
+++ index.js
@@ -2,16 +2,21 @@
 
 const serverless = require('serverless-http');
 
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const app = express();
+
+const { smsHandler } = require("./textHandler");
 
 
 app.get('/', function (req, res) {
 
   res.send('Hello World!')
 
-})
+});
+
+
+app.post("/sms", smsHandler);
 
 
 module.exports.handler = serverless(app);
