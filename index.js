7aefe5bc1e529847892298865a8163b9909c574e
Kevin Quinn <kpquinn2@wisc.edu>
2018-09-26 21:37:35 -0500

    Add prettier for code style

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 // index.js
 
-const serverless = require('serverless-http');
+const serverless = require("serverless-http");
 
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 const { smsHandler } = require("./textHandler");
 
-
-app.post('/sms', function (req, res) {
-
-  res.send('Hello World!!')
-
+app.post("/sms", function(req, res) {
+    res.send("Hello World!!");
 });
 
-
 app.post("/sms", smsHandler);
-
 
 module.exports.handler = serverless(app);
